c1b4c785461f65e565c3f787bf66cc9d6ee56fc4
pete <qingpingjian1985@163.com>
2018-04-20 23:51:31 +0800

    svm first version

--- RawData/WiFiRepo/svmdemo.py
+++ RawData/WiFiRepo/svmdemo.py
@@ -7,6 +7,7 @@ Created on 2018/4/20 20:56
 @file: svmdemo.py
 @software: PyCharm Community Edition
 """
+import math
 import numpy as np
 import pandas as pd
 import timeit
@@ -14,7 +15,16 @@ import timeit
 from sklearn import svm
 
 def accuracy(predictions, labels):
-    return np.mean(np.sqrt(np.sum((predictions - labels)**2, 1)))
+    squareSum = np.sum((predictions-labels)**2, 1)
+    print(type(squareSum))
+    print(squareSum.shape)
+    print(squareSum)
+    c = np.array([v for v in squareSum])
+    print(c.shape, type(c))
+    print(np.sqrt(c))    
+    np.sqrt(np.array([v for v in squareSum]))
+    acc = np.mean([math.sqrt(v) for v in squareSum])
+    return acc
 
 def firstSVM(wfTrain, posTrain, wfTest, posTest, logFlag=False):
     if logFlag:
@@ -40,15 +50,17 @@ if __name__ == "__main__":
     
     # Get train Wi-Fi fingerprints data set    
     trainWifiDF = pd.read_csv(trainWifiFilePath)
-    colsN = trainWifiDF.values.shape[1]
-    wifiTrain = trainWifiDF.ix[:,0:-3]
-    posTrain = trainWifiDF.ix[:, colsN-3:]
-
+    trainWifiInfo = trainWifiDF.values
+    colsN = trainWifiInfo.shape[1]
+    wifiTrain = trainWifiInfo[:,0:-3]
+    posTrain = trainWifiInfo[:, colsN-3:-1]
+    
     # Get test Wi-Fi fingerprints data set
     testWifiDF = pd.read_csv(testWifiFilePath)
-    colsN = testWifiDF.values.shape[1]
-    wifiTest = testWifiDF.ix[:,0:-3]
-    posTest = testWifiDF.ix[:,colsN-3:]
+    testWifiInfo = testWifiDF.values
+    colsN = testWifiInfo.shape[1]
+    wifiTest = testWifiInfo[:,0:-3]
+    posTest = testWifiInfo[:,colsN-3:-1]
 
     # Support Vector Machine for Regression
     firstSVM(wifiTrain, posTrain, wifiTest, posTest, logFlag=True)
