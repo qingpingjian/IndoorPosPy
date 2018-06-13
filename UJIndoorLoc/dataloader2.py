af6a38815930696bdd159d42db57cc546b895568
qingping <qingpingjian1985@163.com>
2018-06-13 22:44:11 +0800

    knn baseline implementation

--- UJIndoorLoc/dataloader2.py
+++ UJIndoorLoc/dataloader2.py
@@ -25,19 +25,21 @@ def load_wifi_data(raw_wifi_file):
     heading_list.extend(ap_heading_list)
     heading_list.extend(part_heading_list)
     wifi_df = pd.read_csv(raw_wifi_file)
-    wifi_dict = {} # {build_id: [features_array, labels_array]}
+    wifi_dict = {} # {build_id: [features_array, labels_array, [coordinate1, coordinate2]]}
     for wifi_record in wifi_df.ix[:, heading_list].values:
         build_id = wifi_record[len(ap_heading_list) - 1 + 4]
         floor_id = wifi_record[len(ap_heading_list) - 1 + 3]
         space_id = wifi_record[len(ap_heading_list) - 1 + 5]
         wifi_info = wifi_record[0:len(ap_heading_list)].astype(float)
         space_label = int((build_id + 1) * 10000 + (floor_id + 1) * 1000 + space_id)
+        coord_meter = (wifi_record[len(ap_heading_list) - 1 + 1], wifi_record[len(ap_heading_list) - 1 + 2])
         if wifi_dict.has_key(build_id):
             tempList = wifi_dict[build_id]
             tempList[0].append(wifi_info)
             tempList[1].append(space_label)
+            tempList[2].append(coord_meter)
         else:
-            wifi_dict[build_id] = [[wifi_info], [space_label]]
+            wifi_dict[build_id] = [[wifi_info], [space_label], [coord_meter]]
     return wifi_dict
 
 
